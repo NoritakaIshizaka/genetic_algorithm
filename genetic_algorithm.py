103c17448388eb8d327a766ccad9966390eeb874
NoritakaIshizaka <nishizaka1015@gmail.com>
2022-07-11 23:12:43 +0900

    add geneticOperate

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,15 +1,18 @@
 import random
-from sympy import Symbol, evaluate
+from sympy import Symbol
 import textwrap
 
 import genetic_algorithm_error
 
 
 class GeneticAlgorithm(object):
-    def __init__( self, dimension, populations = 1000, generations = 100, problem_type = 'min', var_type = 'bin'):
+    def __init__( self, dimension, populations = 100, generations = 100, problem_type = 'min', var_type = 'bin'):
         self.dimension = dimension
         self.populations = populations
         self.generations = generations
+        self._selectRate = 0.5
+        self._crossOverRate = 0.49
+        self._mutateRate = 0.01
         if problem_type != 'min' and problem_type != 'max':
             raise genetic_algorithm_error.ProblemTypeError("set 'min' or 'max'")
         if var_type != 'bin' and var_type != 'float':
@@ -43,11 +46,11 @@ class GeneticAlgorithm(object):
 
 
     def evaluateFitness(self, individuals):
-        evaluateResults = []
+        results = []
         for individual in individuals:
-            evaluateResult = self.fitness.subs([(k, v) for k, v in zip(self.x, individual)])
-            evaluateResults.append([individual, evaluateResult])
-        return evaluateResults
+            result = self.fitness.subs([(k, v) for k, v in zip(self.x, individual)])
+            results.append([individual, result])
+        return results
 
 
     def resolve(self, select_method = 'roulette', crossOver_method = 'one'):
@@ -56,19 +59,36 @@ class GeneticAlgorithm(object):
         elif self.var_type == 'float':
             pass
         for i in range(self.generations):
-            evaluateResults = self.evaluateFitness(individuals=individuals)
-            sort_evaluateResults = sorted(evaluateResults, key=lambda x:x[1], reverse = True)
+            results = self.evaluateFitness(individuals=individuals)
+            sort_results = sorted(results, key=lambda x:x[1], reverse = True)
+            fitnessMin = sort_results[-1][1]
+            if fitnessMin < 0:
+                sort_results = [[sort_result[0], sort_result[1] - (fitnessMin - 1)] for sort_result in sort_results]
+            next_individuals = self.geneticOperate(sort_results)
+            
+
+    def geneticOperate(self, sort_results):
+        next_individuals=[]
+        while True:
+            operationSelector = random.random()
+            if 0.0 < operationSelector <= self._selectRate:
+                self.select(self)
+            elif self._selectRate < operationSelector <= self._selectRate + self._crossOverRate:
+                self.crossOver(self)
+            elif self._selectRate + self._crossOverRate < operationSelector < self._selectRate + self._crossOverRate + self._mutateRate
+                self.matate(self)
+
+
+
+    def select(self, sort_results, method='roulette'):
+        pass
 
 
     def crossOver(self):
         pass
 
 
-    def select(self):
-        pass
-
-
-    def maturate(self):
+    def matate(self):
         pass
 
 
@@ -83,7 +103,7 @@ def main():
     print(ga)
     x = ga.getVariable()
     print(x)
-    f = x[0] + x[1] + x[2]
+    f = (x[0] + x[1] + x[2]) * -1
     ga.setFitness(f)
     print(ga.fitness)
     ga.resolve()
